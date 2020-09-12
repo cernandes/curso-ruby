9c8eeb2719403f72314e12e89f6afad204cdba6f
Claudio <cernandes.silva@gmail.com>
2020-09-11 22:34:44 -0300

    aula 07 POO II

--- missoes_especiais/esportistas.rb
+++ missoes_especiais/esportistas.rb
@@ -1,18 +1,23 @@
-# criar um programa com as classes Esportistas > método competir > "Participando de uma competição", JogadorDeFutebol > método correr > "Correndo atrás da bola", Maratonista > método correr > "Percorrendo o circuito"
+# criar um programa com as classes Esportista, JogadorDeFutebol e Maratonista com os métodos competir e corres.
+# As classes JogadorDeFutebol e Maratonista devem herdar os comportamentos da class Esportista 
+# imprimir as respectivas mensagens: "Participando de uma competição", "Correndo atrás da bola" e "Percorrendo o circuito"
 
 class Esportista
     def competir
         puts "Participando de uma competição"
     end
+    def correr
+      puts "Correndo"  
+    end
 end
 
-class JogadoDeFutebol
+class JogadoDeFutebol < Esportista
     def correr
         puts "Correndo atrás da bola"
     end
 end
 
-class Maratonista
+class Maratonista < Esportista
     def correr
         puts "Percorrendo o circuito"
     end
